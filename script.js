dbd469ee9bb278d67a76cd6b284707990f499c00
Tcharlles <tcharlles3@gmail.com>
2022-05-22 21:25:19 -0300

    flip-ok

---
+++ script.js
@@ -0,0 +1,26 @@
+/*CARDS*/
+
+var card = document.querySelector(".card");
+card.addEventListener("click", function () {
+  card.classList.toggle("is-flipped");
+});
+
+var card2 = document.querySelector(".card2");
+card2.addEventListener("click", function () {
+  card2.classList.toggle("is-flipped");
+});
+
+var card3 = document.querySelector(".card3");
+card3.addEventListener("click", function () {
+  card3.classList.toggle("is-flipped");
+});
+
+var card4 = document.querySelector(".card4");
+card4.addEventListener("click", function () {
+  card4.classList.toggle("is-flipped");
+});
+
+var card5 = document.querySelector(".card5");
+card5.addEventListener("click", function () {
+  card5.classList.toggle("is-flipped");
+});
